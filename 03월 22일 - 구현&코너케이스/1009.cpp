86e4375865bd213ba6c003ed18b7c221c258e317
zziyuni <xhrl6671@naver.com>
2022-03-28 11:57:45 +0900

    Create 1009.cpp
    [구현&코너케이스] 3월 28일

---
+++ 03월 22일 - 구현&코너케이스/1009.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    int t;
+    int a, b;
+    cin >> t;
+    while (t--) {
+        cin >> a >> b;
+        a %= 10;
+        b %= 4;
+        if(b==0){
+            b=4;
+        }
+        if(a==0){
+            cout <<10;
+        }
+        else{
+            int tmp = pow(a,b);
+            cout <<tmp%10<<"\n";
+        }
+
+
+    }
+
+    return 0;
+}
